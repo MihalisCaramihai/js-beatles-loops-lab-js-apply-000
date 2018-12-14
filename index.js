1838a4b2ef8e9061751383086283d9548b60d25e
Mihalis Caramihai <mihalis@caramihai.com>
2018-12-14 01:08:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+for ("Guitar", "Bass Guitar", "Lead Guitar", "Drums"){
+  
+}
+
+function theBeatlesPlay(){
+  var musicians = [];
+  var instruments = [];
+}
+
+function johnLennonFacts(){
+  
+}
+
+function iLoveTheBeatles(){
+  
+}
