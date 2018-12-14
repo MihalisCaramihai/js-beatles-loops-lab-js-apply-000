582e051f83a9fad5ea1b9a73d890e7686b8c89c8
Mihalis Caramihai <mihalis@caramihai.com>
2018-12-14 13:47:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // add solution here
+for ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]{
+  
+}
+
+// for loor
+for (i = 0; i < 5; i++) {
+  text += "The number is " + i + "<br>";
+}
+
+// while loop
+while (condition) {
+  code block to be executed
+}
+
+//do/while loop
+var text = "";
+var i = 0;
+do {
+    text += "The number is " + i;
+    i++;
+}
+while (i < 5);
+//end of loops
+
+
+function theBeatlesPlay(){
+  var musicians = [];
+  var instruments = [];
+}
+
+function johnLennonFacts(){
+  
+}
+
+function iLoveTheBeatles(){
+  
+}
